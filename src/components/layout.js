71b66d519075b9d77e45180268643093c1f248df
John Otander <johnotander@gmail.com>
2019-08-28 19:06:18 -0600

    Add typography and other styles

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,7 +1,20 @@
-import React from 'react'
+/** @jsx jsx */
+import { jsx } from 'theme-ui'
+import { Styled } from 'theme-ui'
 
-export default ({ children }) => (
-  <>
-    {children}
-  </>
-)
+export default ({ children }) => {
+  return (
+    <div
+      sx={{
+        pt: [4, 5, 5],
+        px: 3,
+        m: 'auto',
+        maxWidth: 'container'
+      }}
+    >
+      <Styled.root>
+        {children}
+      </Styled.root>
+    </div>
+  )
+}
--- src/gatsby-plugin-theme-ui/index.js
+++ src/gatsby-plugin-theme-ui/index.js
@@ -1,6 +1,139 @@
 export default {
   colors: {
-    text: '#111',
-    background: '#fff',
+    text: "#000",
+    background: "#fff",
+    primary: "#33e",
+    secondary: "#119",
+    muted: "#f6f6f6",
+    highlight: "#efeffe",
+    gray: "#777",
+    accent: "#609"
   },
-}
+  fonts: {
+    body: "system-ui, sans-serif",
+    heading: "inherit",
+    monospace: "Menlo, monospace"
+  },
+  fontSizes: [12, 14, 16, 20, 24, 32, 48, 64, 72],
+  fontWeights: {
+    body: 400,
+    heading: 500,
+    display: 500
+  },
+  lineHeights: {
+    body: 1.5,
+    heading: 1.25
+  },
+  sizes: {
+    container: 800,
+  },
+  textStyles: {
+    heading: {
+      mt: 4,
+      fontFamily: "heading",
+      fontWeight: "heading",
+      lineHeight: "heading"
+    },
+    display: {
+      variant: "textStyles.heading",
+      fontSize: [5, 6],
+      fontWeight: "display",
+      letterSpacing: "-0.03em",
+      mt: 3
+    }
+  },
+  styles: {
+    Container: {
+      p: 3,
+      maxWidth: 1024
+    },
+    root: {
+      fontFamily: "body",
+      lineHeight: "body",
+      fontWeight: "body"
+    },
+    h1: {
+      variant: "textStyles.display"
+    },
+    h2: {
+      variant: "textStyles.heading",
+      fontSize: 5
+    },
+    h3: {
+      variant: "textStyles.heading",
+      fontSize: 4
+    },
+    h4: {
+      variant: "textStyles.heading",
+      fontSize: 3
+    },
+    h5: {
+      variant: "textStyles.heading",
+      fontSize: 2
+    },
+    h6: {
+      variant: "textStyles.heading",
+      fontSize: 1
+    },
+    p: {
+      fontSize: [2, 3, 4],
+    },
+    ul: {
+      fontSize: [2, 3, 4],
+    },
+    a: {
+      color: "primary",
+      "&:hover": {
+        color: "secondary"
+      }
+    },
+    pre: {
+      fontFamily: "monospace",
+      fontSize: 1,
+      p: 3,
+      color: "text",
+      bg: "muted",
+      overflow: "auto",
+      code: {
+        color: "inherit"
+      }
+    },
+    code: {
+      fontFamily: "monospace",
+      fontSize: 1
+    },
+    inlineCode: {
+      fontFamily: "monospace",
+      color: "secondary",
+      bg: "muted"
+    },
+    table: {
+      width: "100%",
+      my: 4,
+      borderCollapse: "separate",
+      borderSpacing: 0,
+      "th,td": {
+        textAlign: "left",
+        py: "4px",
+        pr: "4px",
+        pl: 0,
+        borderColor: "muted",
+        borderBottomStyle: "solid"
+      }
+    },
+    th: {
+      verticalAlign: "bottom",
+      borderBottomWidth: "2px"
+    },
+    td: {
+      verticalAlign: "top",
+      borderBottomWidth: "1px"
+    },
+    hr: {
+      border: 0,
+      borderBottom: "1px solid",
+      borderColor: "muted"
+    }
+  }
+};
+
